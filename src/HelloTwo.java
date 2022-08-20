a0c30e666be0b70590892384e9bd7858c1709a7c
Artem <leonenkoavfin@gmail.com>
2022-08-20 15:29:53 +0300

    Add date

--- src/HelloTwo.java
+++ src/HelloTwo.java
@@ -1,6 +1,10 @@
+import java.time.LocalDate;
+
 public class HelloTwo {
     public static void main(String[] args) {
         System.out.println("22222222");
         System.out.println("44444");
+
+        System.out.println(LocalDate.now());
     }
 }
